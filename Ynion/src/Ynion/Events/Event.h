fabe8210dee18384618218ca7ba2d066f7ccb42f
guorenY <1404193610@qq.com>
2024-03-22 17:33:41 +0800

    Added events to ImGui layer.

--- Ynion/src/Ynion/Events/Event.h
+++ Ynion/src/Ynion/Events/Event.h
@@ -10,7 +10,7 @@ namespace Ynion {
 		None = 0,
 		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
 		AppTick, AppUpdate, AppRender,
-		KeyPressed, KeyReleased,
+		KeyPressed, KeyReleased, KeyTyped,
 		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 	};
 
--- Ynion/src/Ynion/ImGui/ImGuiLayer.h
+++ Ynion/src/Ynion/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Ynion/Layer.h"
 
+#include "Ynion/Events/ApplicationEvent.h"
+#include "Ynion/Events/KeyEvent.h"
+#include "Ynion/Events/MouseEvent.h"
+
 namespace Ynion {
 
 	class YNION_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Ynion {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
