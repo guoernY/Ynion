b9340e92599ef87553a9f2cc10ef8b7efe4da464
GuorenY <2021302041062@whu.edu.cn>
2024-03-30 19:58:33 +0800

    Added orthographic camera.

--- Ynion/src/Ynion/Application.cpp
+++ Ynion/src/Ynion/Application.cpp
@@ -12,6 +12,7 @@ namespace Ynion {
 	Application* Application::s_Instance = nullptr;
 
 	Ynion::Application::Application()
+		: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 	{
 		YN_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -77,6 +78,8 @@ namespace Ynion {
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
 
+			uniform mat4 u_ViewProjection;
+
 			out vec3 v_Position;
 			out vec4 v_Color;
 
@@ -84,7 +87,7 @@ namespace Ynion {
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -112,12 +115,14 @@ namespace Ynion {
 
 			layout(location = 0) in vec3 a_Position;
 
+			uniform mat4 u_ViewProjection;
+
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -167,15 +172,13 @@ namespace Ynion {
 			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 			RenderCommand::Clear();
 
-			Renderer::BeginScene();
+			m_Camera.SetPosition({ 0.2f, 0.2f, 0.0f });
+			m_Camera.SetRotation(45.0f);
 
-			m_BlueShader->Bind();
-			m_SquareVA->Bind();
-			Renderer::Submit(m_SquareVA);
+			Renderer::BeginScene(m_Camera);
 
-			m_Shader->Bind();
-			m_VertexArray->Bind();
-			Renderer::Submit(m_VertexArray);
+			Renderer::Submit(m_BlueShader, m_SquareVA);
+			Renderer::Submit(m_Shader, m_VertexArray);
 
 			Renderer::EndScene();
 
--- Ynion/src/Ynion/Application.h
+++ Ynion/src/Ynion/Application.h
@@ -13,6 +13,8 @@
 #include "Ynion/Renderer/Buffer.h"
 #include "Ynion/Renderer/VertexArray.h"
 
+#include "Ynion/Renderer/OrthographicCamera.h"
+
 namespace Ynion {
 
 	class YNION_API Application
@@ -44,6 +46,8 @@ namespace Ynion {
 
 		std::shared_ptr<Shader> m_BlueShader;
 		std::shared_ptr<VertexArray> m_SquareVA;
+
+		OrthographicCamera m_Camera;
 	private:
 		static Application* s_Instance;
 	};
