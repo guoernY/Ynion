b5314b69edbb7a46bf026c0f6309d5ef776937a4
GuorenY <2021302041062@whu.edu.cn>
2024-04-02 14:44:40 +0800

    Moved OpenGL shader implementation to OpenGLShader class + added more unifrom functions.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Ynion.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Ynion::Layer
 {
@@ -94,11 +97,11 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Ynion::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Ynion::Shader::Create(vertexSrc, fragmentSrc));
 
 		// Shader of the square
 		// --------------------
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -115,20 +118,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Ynion::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Ynion::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	virtual void OnUpdate(Ynion::Timestep ts) override
@@ -157,13 +162,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Ynion::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Ynion::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f - 1.0f, y * 0.11f - 1.0f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Ynion::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Ynion::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -174,6 +182,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	virtual void OnEvent(Ynion::Event& event) override
@@ -183,7 +194,7 @@ private:
 	std::shared_ptr<Ynion::Shader> m_Shader;
 	std::shared_ptr<Ynion::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Ynion::Shader> m_BlueShader;
+	std::shared_ptr<Ynion::Shader> m_FlatColorShader;
 	std::shared_ptr<Ynion::VertexArray> m_SquareVA;
 
 	Ynion::OrthographicCamera m_Camera;
@@ -191,6 +202,8 @@ private:
 	float m_CameraMoveSpeed = 5.0f;
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 90.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Ynion::Application
