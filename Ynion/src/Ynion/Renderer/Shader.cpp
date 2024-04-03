aac9613438229e831f93c09f699e643255215f69
GuorenY <2021302041062@whu.edu.cn>
2024-04-03 13:40:16 +0800

    Shaders can now be read from files.

--- Ynion/src/Ynion/Renderer/Shader.cpp
+++ Ynion/src/Ynion/Renderer/Shader.cpp
@@ -6,6 +6,18 @@
 
 namespace Ynion {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:	YN_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:	return new OpenGLShader(filepath);
+		}
+
+		YN_CORE_ASSERT(false, "Unkown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
