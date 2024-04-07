724e2c51a99e2c1189013680f3818a04fa894b6b
GuorenY <2021302041062@whu.edu.cn>
2024-04-07 15:35:53 +0800

    Added textures to Renderer2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -7,12 +7,13 @@
 #include "Platform/OpenGL/OpenGLShader.h"
 
 Sandbox2D::Sandbox2D()
-	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
+	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true)
 {
 }
 
 void Sandbox2D::OnAttach()
 {
+	m_CheckerboardTexture = Ynion::Texture2D::Create("assets/textures/Checkerboard.png");
 }
 
 void Sandbox2D::OnDetach()
@@ -30,8 +31,9 @@ void Sandbox2D::OnUpdate(Ynion::Timestep ts)
 
 	Ynion::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	Ynion::Renderer2D::DrawQuad({ -0.5f, -0.5f }, { 0.5f, 0.5f }, m_SquareColor);
-	Ynion::Renderer2D::DrawQuad({ 0.5f, 0.0f }, { 0.5f, 1.5f }, m_SquareColor);
+	Ynion::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, m_SquareColor);
+	Ynion::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, m_SquareColor);
+	Ynion::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
 
 	Ynion::Renderer2D::EndScene();
 }
