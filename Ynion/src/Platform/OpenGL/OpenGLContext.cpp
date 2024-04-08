869e2ecc55c334c58a6e7b6f0caaded1ee22abed
GuorenY <2021302041062@whu.edu.cn>
2024-04-08 15:55:20 +0800

    Added check for OpenGL version

--- Ynion/src/Platform/OpenGL/OpenGLContext.cpp
+++ Ynion/src/Platform/OpenGL/OpenGLContext.cpp
@@ -23,6 +23,15 @@ namespace Ynion {
 		YN_CORE_INFO("	Vendor: {0}", (char*)glGetString(GL_VENDOR));
 		YN_CORE_INFO("	Renderer: {0}", (char*)glGetString(GL_RENDERER));
 		YN_CORE_INFO("	Version: {0}", (char*)glGetString(GL_VERSION));
+
+		#ifdef YN_ENABLE_ASSERTS
+			int versionMajor;
+			int versionMinor;
+			glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+			glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+			YN_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Ynion requires at least OpenGL version 4.5!");
+		#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
