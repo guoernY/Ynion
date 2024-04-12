3f002e4662e42c7e2d2ea8eec4a10d535396763f
GuorenY <2021302041062@whu.edu.cn>
2024-04-12 10:38:08 +0800

    Added basic batching to Renderer2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -79,9 +79,10 @@ void Sandbox2D::OnUpdate(Ynion::Timestep ts)
 	{
 		PROFILE_SCOPE("Renderer Draw");
 		Ynion::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Ynion::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), m_SquareColor);
-		Ynion::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, m_SquareColor);
-		Ynion::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f, glm::vec4(0.8f, 0.8f, 0.9f, 1.0f));
+		//Ynion::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), m_SquareColor);
+		Ynion::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Ynion::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+		//Ynion::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f, glm::vec4(0.8f, 0.8f, 0.9f, 1.0f));
 		Ynion::Renderer2D::EndScene();
 	}
 }
--- Ynion/src/Ynion/Renderer/Buffer.cpp
+++ Ynion/src/Ynion/Renderer/Buffer.cpp
@@ -7,6 +7,18 @@
 
 namespace Ynion {
 
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:	YN_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:	return CreateRef<OpenGLVertexBuffer>(size);
+		}
+
+		YN_CORE_ASSERT(false, "Unkown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
@@ -19,12 +31,12 @@ namespace Ynion {
 		return nullptr;
 	}
 
-	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t count)
 	{
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None:	YN_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-			case RendererAPI::API::OpenGL:	return CreateRef<OpenGLIndexBuffer>(indices, size);
+			case RendererAPI::API::OpenGL:	return CreateRef<OpenGLIndexBuffer>(indices, count);
 		}
 
 		YN_CORE_ASSERT(false, "Unkown RendererAPI!");
