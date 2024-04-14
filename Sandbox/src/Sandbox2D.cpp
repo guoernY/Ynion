2a48ec1705f2bc95e9c7f1bdc33cebdb6579ee26
GuorenY <2021302041062@whu.edu.cn>
2024-04-14 22:00:12 +0800

    Renderer2D: added draw call splitting and stats

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -46,7 +46,7 @@ private:
 #define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { m_ProfileResults.push_back(profileResult); })
 
 Sandbox2D::Sandbox2D()
-	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true)
+	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true), m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
 {
 }
 
@@ -70,6 +70,7 @@ void Sandbox2D::OnUpdate(Ynion::Timestep ts)
 	}
 
 	// Render
+	Ynion::Renderer2D::ResetStats();
 	{
 		PROFILE_SCOPE("Renderer Prep");
 		Ynion::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -85,8 +86,19 @@ void Sandbox2D::OnUpdate(Ynion::Timestep ts)
 		Ynion::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, m_SquareColor);
 		Ynion::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Ynion::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Ynion::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Ynion::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
 		Ynion::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+		Ynion::Renderer2D::EndScene();
+
+		Ynion::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f,0.7f };
+				Ynion::Renderer2D::DrawQuad({ x,y }, { 0.45f, 0.45f }, color);
+			}
+		}
 		Ynion::Renderer2D::EndScene();
 	}
 }
@@ -104,6 +116,13 @@ void Sandbox2D::OnImGuiRender()
 		ImGui::Text(label, result.Time);
 	}
 	m_ProfileResults.clear();
+
+	auto stats = Ynion::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
 
 	ImGui::End();
 }
