f5260f28cd5dcb26ba7ead92501ef49b13ffa95e
GuorenY <2021302041062@whu.edu.cn>
2024-04-15 14:44:06 +0800

    Fixed IndexCount selection in OpenGLRendererAPI::DrawIndexed

--- Ynion/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Ynion/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -58,7 +58,7 @@ namespace Ynion {
 
 	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount)
 	{
-		uint32_t count = indexCount ? vertexArray->GetIndexBuffers()->GetCount() : indexCount;
+		uint32_t count = indexCount ? indexCount : vertexArray->GetIndexBuffers()->GetCount();
 		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
