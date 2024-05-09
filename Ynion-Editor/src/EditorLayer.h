244e4dfe46922b6a32754ad43116580018f7ede5
GuorenY <93963856+guoernY@users.noreply.github.com>
2024-05-09 10:35:36 +0800

    Added New, Open, and Save As file menu options to Ynion-Editor

--- Ynion-Editor/src/EditorLayer.h
+++ Ynion-Editor/src/EditorLayer.h
@@ -18,6 +18,12 @@ namespace Ynion {
 		virtual void OnImGuiRender() override;
 		virtual void OnEvent(Event& e) override;
 	private:
+		bool OnKeyPressed(KeyPressedEvent& e);
+
+		void NewScene();
+		void OpenScene();
+		void SaveSceneAs();
+	private:
 		OrthographicCameraController m_CameraController;
 
 		Ref<Framebuffer> m_Framebuffer;
