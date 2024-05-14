0225facf9dff1d85bc249d79db173fe246c9c984
GuorenY <93963856+guoernY@users.noreply.github.com>
2024-05-14 15:20:41 +0800

    Added physics collider visualization

--- Ynion-Editor/src/EditorLayer.h
+++ Ynion-Editor/src/EditorLayer.h
@@ -24,6 +24,8 @@ namespace Ynion {
 		bool OnKeyPressed(KeyPressedEvent& e);
 		bool OnMouseButtonPressed(MouseButtonPressedEvent& e);
 
+		void OnOverlayRender();
+
 		void NewScene();
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
@@ -66,6 +68,8 @@ namespace Ynion {
 		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 
 		int m_GizmoType = -1;
+
+		bool m_ShowPhysicsColliders = false;
 
 		enum class SceneState
 		{
